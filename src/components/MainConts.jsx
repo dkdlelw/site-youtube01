a259785da53f42afec9cfc4c2a99f5def016695b
dkdlelw <1346zany@gmail.com>
2023-06-29 10:44:32 +0900

    0629

--- src/components/MainConts.jsx
+++ src/components/MainConts.jsx
@@ -2,20 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { Category, Videos } from './';
 
 const MainConts = () => {
-  const [selectCategory, setSelectCategory] = useState();
-  const [videos, setVideos] = useState([]);
+  const [selectCategory, setSelectCategory] = useState('영화');
+  const [videos, setVideos] = useState();
 
   useEffect(() => {
     fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=movie&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=${selectCategory}&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
     )
       .then((response) => response.json())
       .then((result) => {
-        setVideos(result.items)
-        console.log(result.items)
+        setVideos(result.items);
+        //console.log(result.items);
       })
       .catch((error) => console.log(error));
-  }, []);
+  }, [selectCategory]);
+
+  //if (!videos?.items) return <Loader />;
 
   return (
     <main id="main">
@@ -27,10 +29,7 @@ const MainConts = () => {
       </aside>
       <section id="contents">
         <h2>{selectCategory} 유튜버</h2>
-        <Videos 
-          videos={videos}
-          setVideos={setVideos}
-        />
+        {videos && <Videos videos={videos} />}
       </section>
     </main>
   );
